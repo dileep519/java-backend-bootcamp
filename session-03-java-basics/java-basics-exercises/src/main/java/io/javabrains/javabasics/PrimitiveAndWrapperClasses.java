8d2b355be1d1ff2998a717f7b52c230dbc4704c9
CH Dileep Kumar <42892618+dileep519@users.noreply.github.com>
2024-07-04 14:37:41 +0530

    Wrapper class assignment

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/PrimitiveAndWrapperClasses.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/PrimitiveAndWrapperClasses.java
@@ -3,14 +3,21 @@ package io.javabrains.javabasics;
 public class PrimitiveAndWrapperClasses {
     public static void main(String[] args) {
         // Step 1: Declare primitive variables
+        int radius = 360;
+        boolean isNumberEven = true;
 
         // Step 2: Convert primitive variables to wrapper objects
-
+        Integer radiusObj = radius;
+        Boolean isNumberObj = isNumberEven;
+       
         // Step 3: Print the values of the wrapper objects
-
+       System.out.println("Integer object " + radiusObj + " Boolean object " + isNumberObj);
+       
         // Step 4: Convert wrapper objects back to primitive variables
-
+        int newRadius = radiusObj; // or  radiusObj.intValue()
+        boolean newBoolean = isNumberObj;
+        
         // Step 5: Print the values of the primitive variables
-
+        System.out.println("Integer is " + newRadius + " Boolean is " + newBoolean);
     }
 }
