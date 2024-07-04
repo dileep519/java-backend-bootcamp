b4f60c1ff40852d5faa31283ba2503ea46ef133c
CH Dileep Kumar <42892618+dileep519@users.noreply.github.com>
2024-07-04 14:54:59 +0530

    Assignment on instanceof object

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/InstanceOf.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/InstanceOf.java
@@ -3,7 +3,6 @@ package io.javabrains.javabasics;
 public class InstanceOf {
 
     public static void main(String[] args) {
-
         Object[] objects = {
                 10,
                 "Hello",
@@ -12,6 +11,18 @@ public class InstanceOf {
         };
 
         // Challenge: Use instanceof to determine the type of each object in the array
+        
+        for (Object prop : objects){
+            if(prop instanceof Integer){
+                System.out.println("Property " + prop + " is instance of Integer");
+            } else if(prop instanceof String){
+                System.out.println("Property " + prop + " is instance of String");
+            } else if(prop instanceof Boolean){
+                System.out.println("Property " + prop + " is instance of Boolean");
+            } else{
+                System.out.println("Property " + prop + " is instance of Double");
+            }
+        }
 
     }
 }
