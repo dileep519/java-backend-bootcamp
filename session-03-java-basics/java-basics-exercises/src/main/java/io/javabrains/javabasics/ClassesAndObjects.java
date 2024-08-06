2886711424519ac14e5c99b9abf6de828e3f791c
CH Dileep Kumar <42892618+dileep519@users.noreply.github.com>
2024-08-06 22:56:07 +0530

    Implemented car class and it's object

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/ClassesAndObjects.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/ClassesAndObjects.java
@@ -18,9 +18,43 @@ Details: 2020 Toyota Camry
 
  */
 
+class Car {
+    public String make;
+    public String model;
+    public Integer year;
+
+    public Car(String newMake, String newModel, Integer newYear) {
+        make = newMake;
+        model = newModel;
+        year = newYear;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String toString() {
+        return year + " " + make + " " + model;
+    }
+}
+
+
 public class ClassesAndObjects {
 
     public static void main(String[] args) {
+        Car car = new Car("Toyota", "Camry", 2020);
 
+        System.out.println("Make: " + car.getMake());
+        System.out.println("Model: " + car.getModel());
+        System.out.println("Year: " + car.getYear());
+        System.out.println("Details: " + car.toString());
     }
 }
