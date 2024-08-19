c0a86d491c140ce47ddcd90e51d778cd7fa69947
CH Dileep Kumar <42892618+dileep519@users.noreply.github.com>
2024-08-19 12:28:03 +0530

    Added code to check equality and generate hash code

--- session-04-java-collections/java-collections-exercises/src/main/java/io/javabrains/javacollections/EqualsAndHashCode.java
+++ session-04-java-collections/java-collections-exercises/src/main/java/io/javabrains/javacollections/EqualsAndHashCode.java
@@ -6,9 +6,44 @@ Generate equals and hashcode methods using member variables firstName, lastName,
 Compare two instances of Person that have the same data
 */
 
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private Date lastModified;
+
+    public Person(String newFirstName, String newLastName, int newAge, Date modifiedDate) {
+        firstName = newFirstName;
+        lastName = newLastName;
+        age  = newAge;
+        lastModified = modifiedDate;
+    }
+
+    public boolean equals(Object obj) {
+        if(this == obj) {
+            return true;
+        }
+        if(!(obj instanceof Person)){
+            return false;
+        }
+        Person newPerson = (Person) obj;
+        return Objects.equals(firstName, newPerson.firstName)
+                && Objects.equals(lastName, newPerson.lastName)
+                && age == newPerson.age;
+    }
+
+    public int hashCode() {
+        System.out.println(firstName + lastName + age + lastModified);
+        return Objects.hash(firstName, lastName, age);
+    }
+}
+
 public class EqualsAndHashCode {
 
     public static void main(String[] args) {
-
+        Person p1 = new Person("Tester", "one", 23, new Date());
+        Person p2 = new Person("Tester", "one", 23, new Date());
+        System.out.println("Are both equal " + p1.equals(p2) + " Hash code of obj1 " + p1.hashCode() + " Hash code of obj2 " + p2.hashCode());
+        
     }
 }
